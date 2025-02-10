9c9550412dfbd766a512195da50c64d0e3102d17
Gianluca880600 <s273910@studenti.polito.it>
2025-02-10 18:36:54 +0100

    profile finnito

--- gemhunter-mobile/gemhunter/app/(tabs)/profile_page.tsx
+++ gemhunter-mobile/gemhunter/app/(tabs)/profile_page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -10,22 +10,49 @@ import {
   Linking,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { useIsFocused } from "@react-navigation/native";
+import { getAttractions } from "@/dao/attractionsDao";
+import { Attraction } from "@/constants/Attraction";
 
 const Profile = () => {
   const [userData, setUserData] = useState({
-    firstName: "Gianluca",
-    lastName: "Maida",
-    email: "gianlucamaida@studenti.polito.it",
-    phone: "+39 3707005546",
-    github: "https://github.com/Gianluca280600",
-    username: "@g_maida",
-    profileImage: require("../../assets/profile_images/IMG_7471.jpg"), // Percorso relativo al file
+    firstName: "Mario",
+    lastName: "Rossi",
+    email: "mario.rossi@studenti.polito.it",
+    phone: "+39 3707005555",
+    github: "https://github.com/polito-hci-2024/gemhunter",
+    username: "@m_rossi",
+    profileImage: require("../../assets/images/mario.jpg"), // Percorso relativo al file
   });
 
   const getGithubUsername = (url: string) => {
     const parts = url.split("/");
     return parts[parts.length - 1]; // Restituisce l'ultima parte dell'URL
   };
+
+  const isFocus = useIsFocused();
+  const [foundAttractionsCount, setFoundAttractionsCount] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const loadAttractions = async () => {
+      try {
+        if (isFocus) {
+          const results = await getAttractions();
+          setTotal(results.length);
+
+          // Calculate found attractions count
+          const foundCount = results.filter(
+            (attraction: Attraction) => attraction.isFound === 1
+          ).length;
+          setFoundAttractionsCount(foundCount);
+        }
+      } catch (error) {
+        console.error("Failed to load attractions:", error);
+      }
+    };
+    loadAttractions();
+  }, [isFocus]);
 
   return (
     <View style={styles.container}>
@@ -49,6 +76,13 @@ const Profile = () => {
         <View style={styles.inputGroup}>
           <Text style={styles.label}>Last name:</Text>
           <TextInput style={styles.input} value={userData.lastName} editable={false} />
+        </View>
+        <Text style={styles.contactTitle}>Album</Text>
+        <View style={styles.contactGroup}>
+          <Ionicons name="book-outline" size={24} color="green" />
+          <Text style={styles.contactText}>
+            Turin's deck: {foundAttractionsCount}/{total}
+          </Text>
         </View>
         <Text style={styles.contactTitle}>Contacts</Text>
         <View style={styles.contactGroup}>
